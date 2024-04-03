9784c971c215ff557a00f896cc4b2c4e7de7a8eb
khansamaira <khansamairaifanaputri@gmail.com>
2024-04-03 08:35:25 +0700

    first commit

---
+++ dua.py
@@ -0,0 +1,11 @@
+class Sekolah:
+    def __init__():
+        self.nama_sekolah = ''
+        self.alamat = ''
+        self.kodepos = ''
+
+
+class Guru:
+    def __init__():
+        self.nama_guru = ''
+        self.nip = ''
---
+++ satu.py
@@ -0,0 +1,11 @@
+"""
+Contoh skrip python
+"""
+
+print('Praktik Git Pertama')
+
+def kali(a,b):
+    return a * b
+
+if __name__ == '__main__':
+    print(5,7)
